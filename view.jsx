ae58b9c9d8c6d12a38d56456d130508a44169f34
jlowery <jlowery@paypal.com>
2016-04-01 15:47:48 -0500

    Step 4

--- view.jsx
+++ view.jsx
@@ -1,25 +1,48 @@
-var Form = function form(data) {
+var TodoForm = function Todo(props) {
 		return (
-			<form onSubmit={data.submitMethod}>
-				<label htmlFor="name">Who do you want to say hi to?</label>
-				<input id="name" type="text" defaultValue={data.state.name} />
-				<input type="submit" />
+			<form onSubmit={props.addTodo}>
+				<label htmlFor="todo">Add Todo:</label>
+				<input id="todo" name="todo" type="text" />
+				<input type="submit" value="Submit" />
 			</form>
 		);
 	},
+	TodoList = function TodoList(props) {
+		return (
+			<ul>
+				{props.todos.map(function (todo, i) {
+					return <Todo todo={todo} key={i} />;
+				})}
+			</ul>
+		);
+	},
+	Todo = function Todo(props) {
+		var key = props.todo + "-" + props.i;
+		return (
+			<li key={key}>{props.todo}</li>
+		);
+	},
 	App = React.createClass({
-		getInitialState: function () {
-			return { name: 'World' };
+		getInitialState: function getState() {
+			return {
+				todos: ['Learn React Basics']
+			};
 		},
-		setNewName: function (event) {
+		addTodo: function changeName(event) {
 			event.preventDefault();
-			this.setState({ name: event.target.querySelector('#name').value });
+			var newTodo = event.currentTarget.todo.value;
+			this.state.todos.push(newTodo);
+			event.currentTarget.todo.value = '';
+
+			this.setState({
+				todos: this.state.todos
+			})
 		},
 		render: function render() {
 			return (
 				<div>
-					<Form state={this.state} submitMethod={this.setNewName} />
-					<h1>Hello, {this.state.name}!</h1>
+					<TodoForm addTodo={this.addTodo} />
+					<TodoList todos={this.state.todos} />
 				</div>
 			);
 		}
