60061265634b3382d09bccb428f2023a7cc58fb9
jlowery <jlowery@paypal.com>
2016-03-31 23:48:39 -0500

    Step 3

--- view.jsx
+++ view.jsx
@@ -1,24 +1,29 @@
-var App = React.createClass({
-	getInitialState: function () {
-		return { name: 'World' };
-	},
-	setNewName: function (event) {
-		event.preventDefault();
-		this.setState({ name: event.target.querySelector('#name').value });
-	},
-	render: function render() {
+var Form = function form(data) {
 		return (
-			<div>
-				<form onSubmit={this.setNewName}>
-					<label for="name">Who do you want to say hi to?</label>
-					<input id="name" type="text" defaultValue={this.state.name} />
-					<input type="submit" />
-				</form>
-				<h1>Hello, {this.state.name}!</h1>
-			</div>
+			<form onSubmit={data.submitMethod}>
+				<label htmlFor="name">Who do you want to say hi to?</label>
+				<input id="name" type="text" defaultValue={data.state.name} />
+				<input type="submit" />
+			</form>
 		);
-	}
-});
+	},
+	App = React.createClass({
+		getInitialState: function () {
+			return { name: 'World' };
+		},
+		setNewName: function (event) {
+			event.preventDefault();
+			this.setState({ name: event.target.querySelector('#name').value });
+		},
+		render: function render() {
+			return (
+				<div>
+					<Form state={this.state} submitMethod={this.setNewName} />
+					<h1>Hello, {this.state.name}!</h1>
+				</div>
+			);
+		}
+	});
 
 ReactDOM.render(
 	<App />,
